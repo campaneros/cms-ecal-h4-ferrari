9b19ee7ad9e4c4024b5fd09ea4c74cc7432b43f7
root <root@crilin.cern.ch>
2026-06-01 10:55:27 +0200

    .sh stuff

--- process_run.sh
+++ process_run.sh
@@ -2,4 +2,4 @@ source define_envs.sh
 
 cd ${WORKING_DIR}
 
-timeout 120s ./fullexecution.sh "$@" 2>&1 | tee ${LOGS_DIR}/log_run${1}_spill${2}_typeis${3}.log
+timeout 30s ./fullexecution.sh "$@" 2>&1 | tee ${LOGS_DIR}/log_run${1}_spill${2}_typeis${3}.log
